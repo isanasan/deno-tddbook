a9c2c3f3579938218314c77d42c21af5b1dfd70d
isanasan <yfcgpsebp@gmail.com>
2022-03-05 07:37:56 +0900

    add: test

---
+++ tests/money_test.ts
@@ -0,0 +1,8 @@
+import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
+import { Doller } from "../src/money.ts";
+
+Deno.test("Multiplication", () => {
+    const five: Doller = new Doller(2);
+    five.times(5);
+    assertEquals(10, five.amount);
+})
