c74117c42b19325ac21348e74178cc85fd8ca0bf
isanasan <yfcgpsebp@gmail.com>
2022-03-06 06:26:31 +0900

    fix: Cannot redeclare block-scoped variable 'product'.

--- tests/money_test.ts
+++ tests/money_test.ts
@@ -3,8 +3,8 @@ import { Doller } from "../src/money.ts";
 
 Deno.test("Multiplication", () => {
     const five: Doller = new Doller(2);
-    const product: Doller = five.times(5);
-    assertEquals(10, product.amount);
-    const product: Doller = five.times(3);
-    assertEquals(6, product.amount);
+    const product1: Doller = five.times(5);
+    assertEquals(10, product1.amount);
+    const product2: Doller = five.times(3);
+    assertEquals(6, product2.amount);
 })
