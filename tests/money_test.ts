65ef7e2f4b57f3c9e4e2ef799d7ebd450feea291
isanasan <yfcgpsebp@gmail.com>
2022-03-07 05:45:25 +0900

    update: import Expression

--- tests/money_test.ts
+++ tests/money_test.ts
@@ -1,5 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
 import { Money } from "../src/money.ts";
+import { Expression } from "../src/expression.ts";
 
 Deno.test("Multiplication", () => {
   const five: Money = Money.doller(2);
