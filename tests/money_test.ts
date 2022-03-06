ff6f543232f9af7aa67b1ad0959e055408507ce1
isanasan <yfcgpsebp@gmail.com>
2022-03-06 14:05:43 +0900

    delete: reference Doller

--- tests/money_test.ts
+++ tests/money_test.ts
@@ -1,20 +1,19 @@
 import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
-import { Doller } from "../src/money.ts";
 import { Franc } from "../src/money.ts";
 import { Money } from "../src/money.ts";
 
 Deno.test("Multiplication", () => {
     const five: Money = Money.doller(2);
-    assertEquals(new Doller(10), five.times(5));
-    assertEquals(new Doller(6), five.times(3));
+    assertEquals(Money.doller(10), five.times(5));
+    assertEquals(Money.doller(6), five.times(3));
 })
 
 Deno.test("equals", () => {
-    assertEquals(true,new Doller(5).equals(new Doller(5)));
-    assertEquals(false,new Doller(5).equals(new Doller(6)));
+    assertEquals(true,Money.doller(5).equals(Money.doller(5)));
+    assertEquals(false,Money.doller(5).equals(Money.doller(6)));
     assertEquals(true,new Franc(5).equals(new Franc(5)));
     assertEquals(false,new Franc(5).equals(new Franc(6)));
-    assertEquals(false,new Franc(5).equals(new Doller(5)));
+    assertEquals(false,new Franc(5).equals(Money.doller(5)));
 })
 
 Deno.test("FrancMultiplication", () => {
