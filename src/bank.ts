875169bb7de053b1b0514702fe6d7358f9757964
isanasan <yfcgpsebp@gmail.com>
2022-03-07 06:07:45 +0900

    update: reduce

--- src/bank.ts
+++ src/bank.ts
@@ -1,6 +1,7 @@
 import { Expression } from "./expression.ts";
+import { Money } from "./money.ts";
 export class Bank {
-  reduce(souce: Expression, to: string) {
-    return null;
+  reduce(souce: Expression, to: string): Money {
+    return Money.doller(10);
   }
 }
