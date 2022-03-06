8f47269be66ec15e8abceddccdf6cf86af86c357
isanasan <yfcgpsebp@gmail.com>
2022-03-06 14:30:29 +0900

    update: format

--- src/money.ts
+++ src/money.ts
@@ -1,46 +1,43 @@
 export abstract class Money {
-    protected amount: number = 0;
-    abstract times(multiplier: number): Money;
-    abstract currency():string;
-    
-    equals (money: Money):boolean {
-        return this.amount === money.amount
-        && this.constructor.name === money.constructor.name;
-    }
-    
-    static doller(amount:number):Money {
-        return new Doller(amount);
-    }
-    
-    static franc(amount:number):Money {
-        return new Franc(amount);
-    }
+  protected amount: number = 0;
+  abstract times(multiplier: number): Money;
+  abstract currency(): string;
+
+  equals(money: Money): boolean {
+    return this.amount === money.amount &&
+      this.constructor.name === money.constructor.name;
+  }
+
+  static doller(amount: number): Money {
+    return new Doller(amount);
+  }
+
+  static franc(amount: number): Money {
+    return new Franc(amount);
+  }
 }
-export class Doller extends Money{
-    constructor(amount: number) {
-        super();
-        this.amount = amount;
-    }
-    
-    times (multiplier: number): Money
-    {
-        return new Doller(this.amount * multiplier);
-    }
-    
-    currency():string
-    {
-        return "USD";
-    }
+export class Doller extends Money {
+  constructor(amount: number) {
+    super();
+    this.amount = amount;
+  }
+
+  times(multiplier: number): Money {
+    return new Doller(this.amount * multiplier);
+  }
+
+  currency(): string {
+    return "USD";
+  }
 }
-    
-export class Franc extends Money{
-    constructor(amount: number) {
-        super();
-        this.amount = amount;
-    }
-    
-    times (multiplier: number): Money
-    {
-        return new Franc(this.amount * multiplier);
-    }
+
+export class Franc extends Money {
+  constructor(amount: number) {
+    super();
+    this.amount = amount;
+  }
+
+  times(multiplier: number): Money {
+    return new Franc(this.amount * multiplier);
+  }
 }
--- tests/money_test.ts
+++ tests/money_test.ts
@@ -2,26 +2,26 @@ import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
 import { Money } from "../src/money.ts";
 
 Deno.test("Multiplication", () => {
-    const five: Money = Money.doller(2);
-    assertEquals(Money.doller(10), five.times(5));
-    assertEquals(Money.doller(6), five.times(3));
-})
+  const five: Money = Money.doller(2);
+  assertEquals(Money.doller(10), five.times(5));
+  assertEquals(Money.doller(6), five.times(3));
+});
 
 Deno.test("equals", () => {
-    assertEquals(true,Money.doller(5).equals(Money.doller(5)));
-    assertEquals(false,Money.doller(5).equals(Money.doller(6)));
-    assertEquals(true,Money.franc(5).equals(Money.franc(5)));
-    assertEquals(false,Money.franc(5).equals(Money.franc(6)));
-    assertEquals(false,Money.franc(5).equals(Money.doller(5)));
-})
+  assertEquals(true, Money.doller(5).equals(Money.doller(5)));
+  assertEquals(false, Money.doller(5).equals(Money.doller(6)));
+  assertEquals(true, Money.franc(5).equals(Money.franc(5)));
+  assertEquals(false, Money.franc(5).equals(Money.franc(6)));
+  assertEquals(false, Money.franc(5).equals(Money.doller(5)));
+});
 
 Deno.test("FrancMultiplication", () => {
-    const five: Money = Money.franc(2);
-    assertEquals(Money.franc(10), five.times(5));
-    assertEquals(Money.franc(6), five.times(3));
-})
+  const five: Money = Money.franc(2);
+  assertEquals(Money.franc(10), five.times(5));
+  assertEquals(Money.franc(6), five.times(3));
+});
 
-Deno.test("Currency",() => {
-    assertEquals("USD",Money.doller(1).currency());
-    assertEquals("CHF",Money.franc(1).currency());
-})
+Deno.test("Currency", () => {
+  assertEquals("USD", Money.doller(1).currency());
+  assertEquals("CHF", Money.franc(1).currency());
+});
