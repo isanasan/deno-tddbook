289a739cab144e04f702ed50aed469702efa222b
isanasan <yfcgpsebp@gmail.com>
2022-03-07 12:06:54 +0900

    update: Sum constructor

--- src/sum.ts
+++ src/sum.ts
@@ -1,5 +1,5 @@
 import { Money } from "./money.ts";
 export class Sum {
-  public augend: Money | null = null;
-  public addend: Money | null = null;
+  constructor(public augend: Money, public addend: Money) {
+  }
 }
